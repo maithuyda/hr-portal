11aefe5356a2f368dea11fe67cfce26913ed15a2
Macbook pro <macbookpro@MacBook-Pro-cua-Macbook.local>
2022-04-14 10:20:19 +0700

    initial commit

---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Header } from "../../components";
+import "./index.scss";
+
+const HomePage = () => {
+    return (
+        <div className="container">
+            <Header />
+            <p>This is content</p>
+        </div>
+    );
+};
+
+export default HomePage;
