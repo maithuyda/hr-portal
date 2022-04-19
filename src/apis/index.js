5bb9eec54534a3efb661311106ad0d69854468e5
Minh. Vu Cong - CMC Global DU3.1 <vcminh@cmcglobal.vn>
2022-04-19 11:26:43 +0700

    fix conflict and remove .eslintrc.js

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,29 +1,18 @@
 import axios from 'axios';
 import queryString from 'query-string';
 
-<<<<<<< HEAD
-const API_KEY = "64e9c0b550ae1b08c54bf65bb08aba0b";
-const BASE_URL = "https://api.themoviedb.org/3/";
-
-const axiosClient = axios.create({
-  baseURL: BASE_URL,
-  params: {
-    api_key: API_KEY,
-  },
-  headers: {
-    "content-type": "application/json",
-  },
-  paramsSerializer: (params) => queryString.stringify(params),
-=======
+const API_KEY = '64e9c0b550ae1b08c54bf65bb08aba0b';
 const BASE_URL = 'https://api.themoviedb.org/3/';
 
 const axiosClient = axios.create({
     baseURL: BASE_URL,
+    params: {
+        api_key: API_KEY,
+    },
     headers: {
         'content-type': 'application/json',
     },
     paramsSerializer: (params) => queryString.stringify(params),
->>>>>>> 127a2001110efeb4569c7fd63c37aeb0f0eb0135
 });
 
 export default axiosClient;
